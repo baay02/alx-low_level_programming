ccde23842ac6bb03b723a786fb69d7f13ff1935e
baay02 <baaybilya25@yqhoo.com>
2022-09-08 13:42:46 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio>
-
+#include <stdio.h>
 /**
-* main - print the string i the put function
+* main - print the sring in the function
 *
-* Discription: using the main function
-* this program prints "programming is the building a multilingual puzzle
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-        puts("\"programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
