e1e8a02caf44956fa40fdf09451a5e6ffa720b96
baay02 <baaybilya25@yqhoo.com>
2022-09-08 11:40:29 -0700

    C coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+* main - print the string i the put function
+*
+* Discription: using the main function
+* this program prints "programming is the building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        puts("\"programming is like building a multilingual puzzle");
+        return (0);
+}
