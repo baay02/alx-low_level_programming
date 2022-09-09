cbb22de30c88f99593afb77248cd301f0e08084b
baay02 <baaybilya25@yqhoo.com>
2022-09-08 22:26:24 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/**
-* main - print the string in the put fubction
+**/
+* main - print the string in the put function
 *
 * Description: using the main function
-"
 * this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
