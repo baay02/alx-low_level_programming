5ea4a1f1dd756a78c1c2e8d4a442a9a5dbdc60e9
baay02 <baaybilya25@yqhoo.com>
2022-09-09 00:57:42 -0700

    puts fuction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-**/
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
+/**
+* Description: main - prints out a string to stdout.
+* Return: 0 if success.
 */
 int main(void)
 {
-	printf("\"programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
