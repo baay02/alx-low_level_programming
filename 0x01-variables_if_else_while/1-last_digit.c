dcbc73b3e5a958240c587e6158a8335daa6866e2
baay02 <baaybilya25@yqhoo.com>
2022-09-10 08:52:08 -0700

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+	for ( low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
+	return (0);
+}
+
