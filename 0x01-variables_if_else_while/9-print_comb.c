e8afca7e05c3adbfff7968f70ec250a1aa083610
baay02 <baaybilya25@yqhoo.com>
2022-09-11 14:55:18 -0700

    comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print id the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b <= 57; b++)
+	{
+		putchar(b);
+		if (b != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
