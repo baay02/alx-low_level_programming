9b30cc2506206d16d48cf681796ef09ea140942d
baay02 <baaybilya25@yqhoo.com>
2022-09-11 13:36:23 -0700

    nuberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or  egative
+ * Return: 0
+ */
+int main(void)
+{
+	int k;
+	for (k = '0' ; k <= '9' ; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
+
