d35520ee96f64bb57ea965bc897411a0f93d8eb2
baay02 <baaybilya25@yqhoo.com>
2022-09-12 04:38:33 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print piece of art is useful - Dora Korfar
+ *
+ * Description: using the main function
+ * this program prints "that piece if of art is useful
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Kofar, 2015-10-19\n", 59);
+
+	return (0);
+}
+
