ebfe74d3adce6d0fe92ca26a26837b1ff5043328
baay02 <baaybilya25@yqhoo.com>
2022-09-12 09:46:25 -0700

    comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0 ; digit2 < 8 ; digit1++)
+	for (digit1 = 0; digit2 < 8; digit1++)
 	{
-		for (digit2 = digit1 + 1 ; digit2 < 9 ; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (digit3 = digit2 + 1 ; digit3 < 10 ; digit3++)
+			for (digit3 = digit2 + 1; d7igit3 < 10;  digit3++)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
@@ -29,6 +29,7 @@ int main(void)
 		}
 	}
 	putchar('\n');
+	
 	return (0);
 }
 
