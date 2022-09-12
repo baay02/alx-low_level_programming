4bfa06faf73e7aa143d175280a41dcbcd400a145
baay02 <baaybilya25@yqhoo.com>
2022-09-12 08:51:38 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - print piece of art is useful - Dora Korfar
  *
@@ -9,6 +9,7 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korfar, 2015-10-19\n", 59);
+
 	return (1);
 }
 
