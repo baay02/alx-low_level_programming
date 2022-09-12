f1a206e62ad56f7af367f234eedbb9e0174d5a74
baay02 <baaybilya25@yqhoo.com>
2022-09-12 01:34:57 -0700

    comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - print the combination of two seperated by comma
+ *
+ * Description: using the main function
+ * this program prints " different of two possible numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int digit1, digit2, digit3;
+
+	for (digit1 = 0 ; digit2 < 8 ; digit1++)
+	{
+		for (digit2 = digit1 + 1 ; digit2 < 9 ; digit2++)
+		{
+			for (digit3 = digit2 + 1 ; digit3 < 10 ; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
