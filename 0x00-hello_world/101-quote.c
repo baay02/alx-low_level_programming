9f2eebf013205b5c292df1b83d7e5b21b7b377c4
baay02 <baaybilya25@yqhoo.com>
2022-09-12 08:34:19 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,21 +4,12 @@
  *
  * Description: using the main function
  * this program prints "Programming is like  building a multilingual puzzle
- * Return (0);
+ * Return (1);
  */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
-	printf("Size of a char; %ld byte(s)\n", sizeof(c));
-	printf("Size of an int; %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int; %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int; %ld byte(s)\n", sizeof(lli);
-	printf("Size of  float; %ld byte(s)\n", sizeof(f);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korfar, 2015-10-19\n", 59);
+	return (1);
 }
+
 
