aa5f8e5da0de9caff5a12be48afa2801f7ffe90c
baay02 <baaybilya25@yqhoo.com>
2022-09-11 22:12:33 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (digit = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
