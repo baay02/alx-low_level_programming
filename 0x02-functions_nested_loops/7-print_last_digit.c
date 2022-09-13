954ce5dfd967c8307f8847fc2e51028d48ad2f53
baay02 <baaybilya25@yqhoo.com>
2022-09-13 14:40:16 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,7 @@ int print_last_digit(int n)
 	if (last_digit < 0)
 		last_digit *= -1;
 
-		_putchar(last_digit + '0');
+	_putchar(last_digit + '0');
 
-		return (last_digit);
+	return (last_digit);
 }
