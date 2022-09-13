e486f6cac27d9ced83ae89a47fa34998317544cb
baay02 <baaybilya25@yqhoo.com>
2022-09-13 16:39:25 -0700

    sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - function to check the sign od number
- * @c: is the int that will use for the argument of the function
+ * @n: is the int that will use for the argument of the function
  * Return: 0
  */
 
