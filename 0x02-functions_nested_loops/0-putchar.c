d6d71296d40ac2ffbdd909191abd9b65c861af02
baay02 <baaybilya25@yqhoo.com>
2022-09-13 07:27:23 -0700

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * main - print "Holberton" followed by a new line.
+ * main - print "putchar" followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
