52f79ede10566dc5ac227271f4b7c3089ac77acc
baay02 <baaybilya25@yqhoo.com>
2022-09-13 12:33:12 -0700

    print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - function to check the sign od number
+ * @c: is the intthat will use for the argument of a function
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if ( > 0)
+	{
+		_putchar('+');
+		return (1);
+}
+	else if (n < 0)
+{
+	_putchar('-');
+	return (-1);
+}
+	else 
+{
+	_putchar('0');
+	return (0);
+}
+}
