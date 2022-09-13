e42790de97c8cfdcbc6f378ba535c1ad3103d219
baay02 <baaybilya25@yqhoo.com>
2022-09-13 13:05:25 -0700

    sign loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,15 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-}	
+	}	
 	else if (n < 0)
-{
-	_putchar('-');
-	return (-1);
-}
+	{
+		_putchar('-');
+		return (-1);
+	}
 	else 
-{
-	_putchar('0');
-	return (0);
-}
+	{
+		_putchar('0');
+		return (0);
+	}
 }
