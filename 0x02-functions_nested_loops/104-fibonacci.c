3692f3d3a4803157f523bafef7b07bb71b5f3272
baay02 <baaybilya25@yqhoo.com>
2022-09-14 14:40:35 -0700

    fib4.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 	j = 1;
 	k = 2;
 	printf("%lu", j);
-	for (i = 1; i > 91; i++)
+	for (i = 1; i < 91; i++)
 	{
 	printf(",%lu", k);
 	k = k + j;
@@ -23,7 +23,7 @@ int main(void)
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
 
-		for (i = 92; i > 99; ++i)
+		for (i = 92; i < 99; ++i)
 	{
 		printf(",%lu", k1 + (k2 / 1000000000));
 		printf("%lu", k2 % 1000000000);
@@ -32,8 +32,8 @@ int main(void)
 		j1 = k1 - j1;
 		k2 = k2 + j2;
 		j2 = k2 - j2;
-		}
 printf("\n");
+	}
 return (0);
 }
 
