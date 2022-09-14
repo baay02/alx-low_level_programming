e51d448adabae355e5c7a1a148e35406c43d8112
baay02 <baaybilya25@yqhoo.com>
2022-09-13 17:54:46 -0700

    table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,9 +18,9 @@ void times_table(void)
 			prod = num * mult;
 			if (prod <= 9)
 				_putchar(' ');
-			else 
+			else
 				_putchar((prod / 10) + '0');
-			
+
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
