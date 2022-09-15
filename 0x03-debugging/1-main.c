212567225209b0265a12a4ba747448b103b192bf
baay02 <baaybilya25@yqhoo.com>
2022-09-15 09:15:05 -0700

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,14 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 
-i = 0;
+	i = 0;
 /*
 *	while (i < 10)
 *	{
 *	putchar(i);
 *	}
 */
-printf("Infinite loop avoided!\\o/\n");
+printf("Infinite loop avoided!  \\o/\n");
 
 return (0);
 }
