29f68ed37cf37ad92b6b90ef3826d564ad225e15
baay02 <baaybilya25@yqhoo.com>
2022-09-16 12:56:55 -0700

    more numbs

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,10 +17,10 @@ void more_numbers(void)
 		{
 			if (n2 >= 9)
 			{
-				putchar((n2 / 10) + '0');
-				putchar((n2 % 10) + '0');
+				_putchar((n2 / 10) + '0');
+				_putchar((n2 % 10) + '0');
 			}
-			putchar(10);
+			_putchar(10);
 		}
 	}
 }
