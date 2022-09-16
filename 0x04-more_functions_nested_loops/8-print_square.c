91710a47f338e00d6fb52fe7da1167761b12cec0
baay02 <baaybilya25@yqhoo.com>
2022-09-16 07:03:20 -0700

    square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - a function that peints a square, followed by new line
+ * @size: size of both width and lenght
+ * Return: a square made of '#'
+ */
+
+void print_square(int size)
+{
+	int co, ro;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+			_putchar('#');
+			for (ro = 2; ro <= size; ro++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+
+
