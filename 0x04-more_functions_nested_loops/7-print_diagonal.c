8f49963a370ead065ad629910e9033160b0d228f
baay02 <baaybilya25@yqhoo.com>
2022-09-16 10:56:46 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,28 +4,29 @@
 /**
  * print_diagonal - prints a diagonal
  * @n: parameter
- * Return: return nothing
+ * Return: returns nothing
  */
 
-void print_diagonal(void)
+void print_diagonal(int n)
 {
-	int len, space;
+	int l, sp;
 
 	if (n > 0)
 	{
-		for (len = 0; len < n; len++)
+		for (l = 0; l < n; l++)
 		{
-			for (space = 0; space < len; space++)
+			for (sp = 0; sp < l; sp++)
 			{
 				putchar(' ');
 			}
+
 			putchar('\\');
 
-			if (len == (n - 1))
+			if (l == (n - 1))
 			{
 				continue;
 			}
-			putchar('\n');
+			putchar('\n')
 		}
 	}
 	putchar('\n');
