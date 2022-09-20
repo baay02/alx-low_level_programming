e46d3466c486921d99e2103cbce2ec6a10314e55
baay02 <baaybilya25@yqhoo.com>
2022-09-20 02:05:27 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,17 +23,14 @@ int _atoi(char *s)
 
 	while (i < len && f == 0)
 	{
-		if (s[i] == '_')
+		if (s[i] == '-')
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '0';
+			digit = s[i] + '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 - digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
