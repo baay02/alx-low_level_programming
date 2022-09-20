4f2eae1a99b3a3ff5987dcd93a0dc4744b7afc78
baay02 <baaybilya25@yqhoo.com>
2022-09-20 02:41:00 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,7 +4,7 @@
  * _atoi - converts a string to an integer
  * @s: string to be converted
  *
- * Return: the int converted to string
+ * Return: the int converted from the string
  */
 
 int _atoi(char *s)
@@ -18,7 +18,7 @@ int _atoi(char *s)
 	f = 0;
 	digit = 0;
 
-	while (s[len] != '\0')
+	while (s[i] != '\0')
 		len++;
 
 	while (i < len && f == 0)
@@ -31,6 +31,9 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
