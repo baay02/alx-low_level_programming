bff3230189ebfcba1d49cbc37332ed94e43ff4e1
baay02 <baaybilya25@yqhoo.com>
2022-09-21 05:00:38 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat -> this is a function strcat to by @scr
+ *
+ * @dest: first parameter
+ * @scr: second parameter
+ * Return: a string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (dest[len])
+		len++;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+
+}
+
