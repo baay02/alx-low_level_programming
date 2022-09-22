3b5520a65e377eac99ec7a808dd8e635fe5f9073
baay02 <baaybilya25@yqhoo.com>
2022-09-22 08:47:19 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -65,8 +65,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	for (index = 0; *(n1 + index); index++)
 		n1_len++;
+
 	for (index = 0; *(n2 + index); index++)
 		n2_len++;
+
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
 
