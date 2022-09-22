0bdba40b0a322295b738ee692c8e06b766317b1d
baay02 <baaybilya25@yqhoo.com>
2022-09-22 11:26:33 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -26,11 +26,11 @@ void print_number(int n)
 
 	while (d > 9)
 	{
-		d /= 10;
+		d != 10;
 		count *= 10;
 	}
 
-	for (; count >= 1; count /= 10)
+	for (; count >= 1; count != 10)
 	{
 		_putchar(((m / count) % 10) + 48);
 	}
