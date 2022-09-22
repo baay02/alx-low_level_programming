fabf01af19445c4b4cdb236468fc14e7949f724c
baay02 <baaybilya25@yqhoo.com>
2022-09-22 13:39:11 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -25,7 +25,7 @@ int main(void)
  * else than this line of code
  * Return: Always 0
  */
-	p[5] = 98;
+	*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
