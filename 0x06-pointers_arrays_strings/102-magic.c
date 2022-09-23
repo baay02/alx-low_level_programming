8050c40a15797a6741369b089c2629ba43d5e776
baay02 <baaybilya25@yqhoo.com>
2022-09-23 11:44:54 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - using main function
+ * @void: the encoded function
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
 	int n;
@@ -9,15 +16,17 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 /*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
+* write your line of code here...
+* Remember:
+* you are not allowed to use a
+* you are not allowed to modify p
+* only one statement
+* you are not allowed to code anything else than line of code
+*/
+
 	p[5] = 98;
-	/* ...so that this prints 98\n */
+	/* ...so that this print 98'\n' */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
+
 }
