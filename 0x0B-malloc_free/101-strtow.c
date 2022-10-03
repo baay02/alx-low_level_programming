238ca48f431cde3d827378dffa2336f60571d8dd
baay02 <baaybilya25@yahoo.com>
2022-10-03 14:29:36 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,109 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - split string into words
+ * @str: string of word to be splitted
+ * Return: double pointer string
+ */
+char **strtow(char *str)
+{
+
+	char **ptr;
+	int i, k, len, start, end, j = 0;
+	int words = countWords(str);
+
+	if (!str || !countWords(str))
+		return (NULL);
+	ptr = malloc(sizeof(char *) * (words + 1));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < words; i++)
+	{
+		start = startIndex(str, j);
+		end = endIndex(str, start);
+		len = end - start;
+		ptr[i] = malloc(sizeof(char) * (len + 1));
+		if (!ptr[i])
+		{
+			i -= 1;
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for (k = 0; k < len; k++)
+			ptr[i][k] = str[start++];
+		ptr[i][k++] = '\0';
+		j = end + 1;
+	}
+	ptr[i] = NULL;
+	return (ptr);
+}
+
+/**
+ * isSpace - determine if character is space or not
+ * @c: input character
+ * Return: 1 if true, 0 if false
+ */
+int isSpace(char c)
+{
+	return (c == ' ');
+}
+
+/**
+ * startIndex - returns first index of non-space character
+ * @s: input string
+ * @index: starting index
+ * Return: index is first non-space char
+ */
+int startIndex(char *s, int index)
+{
+	while (isSpace(*(s + index)))
+		index++;
+	return (index);
+}
+
+/**
+ * endIndex - returns last index of non-space char
+ * @s: input string
+ * @index: starting index
+ * Return: index of last index of non-space char
+ */
+
+int endIndex(char *s, int index)
+{
+	while (!isSpace(*(s + index)))
+		index++;
+	return (index);
+}
+
+/**
+ * countWords - counts numbers of words in a string
+ * @s: input string
+ * Return: number of words
+ */
+
+int countWords(char *s)
+{
+	int wordOn = 0;
+	int words = 0;
+
+	while (*s)
+	{
+	if (isSpace(*s) && wordOn)
+		wordOn = 0;
+	else if (!isSpace(*s) && !wordOn)
+	{
+		wordOn = 1;
+		words++;
+	}
+	s++;
+}
+return (words);
+}
+
