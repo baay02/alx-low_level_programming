4cfafd6f917c5f489ced769c3d924242a86f8915
baay02 <baaybilya25@yahoo.com>
2022-10-02 22:44:56 -0700

    memory allocation commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - creates an array if characters
+ * and intialize it with a specific character
+ * @size: size of array
+ * @c: character array to fill with
+ * Return: string of characters
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * size);
+	if (!size || !ptr)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
+}
+
