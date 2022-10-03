05469e662858a70a76864bbee945168dee22ec00
baay02 <baaybilya25@yahoo.com>
2022-10-02 23:38:38 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,7 +33,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 	cat = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2)) + 1);
 	if (!cat)
 		return (NULL);
