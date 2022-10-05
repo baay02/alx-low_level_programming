6402a484cdbde8c82a482b34fdf92611aabd0123
baay02 <baaybilya25@yahoo.com>
2022-10-05 00:40:53 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -23,7 +23,7 @@ char **strtow(char *str)
 	{
 		start = startIndex(str, j);
 		end = endIndex(str, start);
-		len = start - end;
+		len = start + end;
 		ptr[i] = malloc(sizeof(char) * (len + 1));
 		if (!ptr[i])
 		{
