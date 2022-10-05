994bfb1c88ff810afc82a9f08fc8633c975706d4
baay02 <baaybilya25@yahoo.com>
2022-10-04 22:54:06 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -24,7 +24,7 @@ char **strtow(char *str)
 		start = startIndex(str, j);
 		end = endIndex(str, start);
 		len = end - start;
-		ptr[i] = malloc(sizeof(char) * (len + 1));
+		ptr[i] = malloc(sizeof(char));
 		if (!ptr[i])
 		{
 			i -= 1;
@@ -106,4 +106,4 @@ int countWords(char *s)
 }
 return (words);
 }
-
+ 
