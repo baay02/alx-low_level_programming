6152374373bfaeff08dd736401742f8a0236a3d7
baay02 <baaybilya25@yahoo.com>
2022-10-12 08:42:08 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		if (array != NULL && cmp != NULL)
 		{
-			while (i < size)
+			while (i <= size)
 			{
 				if (cmp(array[i]))
 						return (i);
