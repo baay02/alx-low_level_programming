47ba7bc28bf3bfb0d6d68c37aa3aebe4bb2f343a
baay02 <baaybilya25@yahoo.com>
2022-10-13 06:21:01 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			string = va_arg(args, char *);
 			if (string == NULL)
-				printf("%s", "(nil");
+				printf("%s", "(nil)");
 			else
 				printf("%s", string);
 			if (i != n - 1 && separator != NULL)
