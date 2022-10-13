e7df1d57efadaaf77cf953dbdc11c32162249ed8
baay02 <baaybilya25@yahoo.com>
2022-10-13 13:01:07 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,11 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
-	while (i < 5)
+	i = 0;
+
+	while (ops[i].op)
 	{
-		if (!strcmp(ops[i].op, s))
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
