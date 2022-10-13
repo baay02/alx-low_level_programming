6878f4e0183485c3ff1b9048c2a30acb9bcae244
baay02 <baaybilya25@yahoo.com>
2022-10-13 13:09:38 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -42,6 +42,11 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -53,6 +58,11 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
 
