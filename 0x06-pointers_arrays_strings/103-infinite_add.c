13594abad521de15e3a3086371959a817e9259c2
baay02 <baaybilya25@yahoo.com>
2022-10-15 10:50:54 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,81 +1,80 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
- /**
-  * isPrintableASCII - determines if n is a printable ASCII char
-  * @n: integer
-  * Return: 1 if true, 0 if false
-  */
-int isPrintableASCII(int n)
-{
-	return (n >= 32 && n <= 126);
-}
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
 
- /**
-  * printHexes - print hex values for string b in formatted form
-  * @b: string to print
-  * @start: starting position
-  * @end: ending position
-  */
-void printHexes(char *b, int start, int end)
+void rev_string(char *n)
 {
 	int i = 0;
+	int j = 0;
+	char temp;
 
-	while (i < 10)
+	while (*(n + i) != '\0')
 	{
-		if (i < end)
-			printf("%02x", *(b + start + i));
-		else
-			printf("  ");
-		if (i % 2)
-			printf(" ");
 		i++;
 	}
-}
+	i--;
 
- /**
-  * printASCII - print ascii values for string b,
-  * formatted to replace nonprintable chars with '.'
-  * @b: string to print
-  * @start: starting position
-  * @end: ending position
-  */
-
-void printASCII(char *b, int start, int end)
-{
-	int ch, i = 0;
-
-	while (i < end)
+	for (j = 0; j < i; j++, i--)
 	{
-		ch = *(b + i + start);
-		if (!isPrintableASCII(ch))
-			ch = 46;
-		printf("%c", ch);
-		i++;
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
 /**
- * print_buffer - prints a buffer
- * @b: string
- * @size: size of buffer
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
-void print_buffer(char *b, int size)
-{
-	int start, end;
 
-	if (size > 0)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		for (start = 0; start < size; start += 10)
-		{
-			end = (size - start < 10) ? size - start : 10;
-			printf("%08x: ", start);
-			printHexes(b, start, end);
-			printASCII(b, start, end);
-			printf("\n");
-		}
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	else
-		printf("\n");
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
+
