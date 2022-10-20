03a9eef2e8d4ad89c2ae512bff434f433daa0f47
baay02 <baaybilya25@yahoo.com>
2022-10-20 04:38:01 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - returns th number of elements in a linked list_t list
+ * @h: head of the linked list
+ *
+ * Return: number of elements in list_t
+ */
+
+size_t list_len(const list_t *h)
+{
+
+	register unsigned int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
