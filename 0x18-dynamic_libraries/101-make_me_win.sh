ef064639313b4f095fd935d22f9be7029d7bc487
baay02 <baaybilya25@yahoo.com>
2022-12-12 21:39:43 -0800

    final task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/baay02/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
-export LD_PRELOAD=../hacked.so
+wget -P /tmp/ https://raw.github.com/baay02/alx-low_level_programming/master/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
